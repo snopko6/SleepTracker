304f588b6539d5b82b5faabc8cf84a68b6b21759
snopko6 <snopko6@stud.uniza.sk>
2023-05-09 19:54:15 +0200

    Šípka naspäť a nastavenie názvu akitivity Historia

--- app/src/main/java/uniza/fri/snopko/robert/sleeptracker/Historia.kt
+++ app/src/main/java/uniza/fri/snopko/robert/sleeptracker/Historia.kt
@@ -2,10 +2,33 @@ package uniza.fri.snopko.robert.sleeptracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+
+/*
+https://developer.android.com/develop/ui/views/components/menus
+https://developer.android.com/reference/android/app/ActionBar
+*/
 
 class Historia : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_historia)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setTitle(R.string.historiaNazov)
+        }
+    }
+
+    /*
+    https://stackoverflow.com/questions/70319639/android-studio-override-fun-onoptionsitemselected-not-navigating-to-second
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
