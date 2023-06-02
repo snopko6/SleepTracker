c8d7656365418a5ea5cd76e84974e4576e2a88e8
snopko6 <snopko6@stud.uniza.sk>
2023-06-02 22:55:50 +0200

    Ďaľšie vizuálne úpravy

--- app/src/main/java/uniza/fri/snopko/robert/sleeptracker/MainActivity.kt
+++ app/src/main/java/uniza/fri/snopko/robert/sleeptracker/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import uniza.fri.snopko.robert.sleeptracker.databinding.ActivityMainBinding
 
@@ -22,14 +20,8 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_statistika, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        supportActionBar?.title = "Sleep Tracker"
         navView.setupWithNavController(navController)
     }
 }
