b6cabc4f91a4ba6248d5f8e85d3a99c8a248f8cb
snopko6 <snopko6@stud.uniza.sk>
2023-06-06 16:28:22 +0200

    Vymazanie nepotrebných metód v HistoriaViewModel

--- app/src/main/java/uniza/fri/snopko/robert/sleeptracker/ui/statistika/historia/HistoriaViewModel.kt
+++ app/src/main/java/uniza/fri/snopko/robert/sleeptracker/ui/statistika/historia/HistoriaViewModel.kt
@@ -3,9 +3,6 @@ package uniza.fri.snopko.robert.sleeptracker.ui.statistika.historia
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import uniza.fri.snopko.robert.sleeptracker.databaza.Spanok
 import uniza.fri.snopko.robert.sleeptracker.databaza.SpanokDatabase
 import uniza.fri.snopko.robert.sleeptracker.databaza.SpanokRepository
@@ -18,13 +15,5 @@ class HistoriaViewModel(application: Application) : AndroidViewModel(application
         val spanokDao = SpanokDatabase.getDatabase(application).spanokDao()
         repository = SpanokRepository(spanokDao)
         spanky = repository.nacitajSpanky
-    }
-
-    fun pridajSpanok(spanok: Spanok) = viewModelScope.launch(Dispatchers.IO) {
-        repository.pridajSpanok(spanok)
-    }
-
-    fun vymazVsetkySpanky() = viewModelScope.launch(Dispatchers.IO) {
-        repository.vymazVsetkySpanky()
     }
 }
