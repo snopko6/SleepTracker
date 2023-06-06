9ab42e3f530c69db1035d17347bebfeded4e541d
snopko6 <snopko6@stud.uniza.sk>
2023-06-06 23:13:15 +0200

    Použitie SharedPreferences na uloženie stavu prepínača a zvolených hodnôt v Spinner-och

--- app/src/main/java/uniza/fri/snopko/robert/sleeptracker/ui/notifikacie/NotifikacieFragment.kt
+++ app/src/main/java/uniza/fri/snopko/robert/sleeptracker/ui/notifikacie/NotifikacieFragment.kt
@@ -1,9 +1,12 @@
 package uniza.fri.snopko.robert.sleeptracker.ui.notifikacie
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -13,7 +16,14 @@ import uniza.fri.snopko.robert.sleeptracker.databinding.FragmentNotificationsBin
 class NotifikacieFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
+    private lateinit var sharedPreferences: SharedPreferences
+
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,33 +34,66 @@ class NotifikacieFragment : Fragment() {
         val notifikacieViewModel =
             ViewModelProvider(this)[NotifikacieViewModel::class.java]
 
-        val prepinac = binding.prepinacNotifikacie
-        val predSpanimTextView = binding.predSpanimTextView
-        val predZobudenimTextView = binding.predZobudenimTextView
+        val prepinacNotifikacie = binding.prepinacNotifikacie
         val minutyPredSpankom = binding.minutyPredSpankom
-        val minutyPredZobudenim = binding.minutyPredZobudenim
+        val minutyPoZobudeni = binding.minutyPoZobudeni
         val casy = resources.getStringArray(R.array.casy)
 
         val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, casy)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         binding.minutyPredSpankom.adapter = adapter
-        binding.minutyPredZobudenim.adapter = adapter
+        binding.minutyPoZobudeni.adapter = adapter
 
+        prepinacNotifikacie.isChecked = sharedPreferences.getBoolean("prepinacNotifikacie", false)
+        nastavViditelnost(prepinacNotifikacie.isChecked)
+        minutyPredSpankom.setSelection(sharedPreferences.getInt("minutyPredSpankom", 0))
+        minutyPoZobudeni.setSelection(sharedPreferences.getInt("minutyPoZobudeni", 0))
+
+        prepinacNotifikacie.setOnCheckedChangeListener { _, jeStlaceny ->
+            sharedPreferences.edit().putBoolean("prepinacNotifikacie", jeStlaceny).apply()
+            nastavViditelnost(jeStlaceny)
+        }
+        //https://stackoverflow.com/questions/46447296/android-kotlin-onitemselectedlistener-for-spinner-not-working
+        minutyPredSpankom.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                sharedPreferences.edit().putInt("minutyPredSpankom", position).apply()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+        minutyPoZobudeni.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                sharedPreferences.edit().putInt("minutyPoZobudeni", position).apply()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
         return binding.root
     }
 
     private fun nastavViditelnost(viditelne: Boolean) {
         if (viditelne) {
+            binding.minutyPredSpankom.visibility = View.VISIBLE
+            binding.minutyPoZobudeni.visibility = View.VISIBLE
             binding.predSpanimTextView.visibility = View.VISIBLE
-            binding.minutyPredZobudenim.visibility = View.VISIBLE
-            binding.predSpanimTextView.visibility = View.VISIBLE
-            binding.predZobudenimTextView.visibility = View.VISIBLE
+            binding.poZobudeniTextView.visibility = View.VISIBLE
         } else {
             binding.minutyPredSpankom.visibility = View.GONE
-            binding.minutyPredZobudenim.visibility = View.GONE
+            binding.minutyPoZobudeni.visibility = View.GONE
             binding.predSpanimTextView.visibility = View.GONE
-            binding.predZobudenimTextView.visibility = View.GONE
+            binding.poZobudeniTextView.visibility = View.GONE
         }
     }
 
